561a20378084d5ffcee3a7f6359057d525444f60
valera <v752433@icloud.com>
2020-05-20 03:23:59 +0300

    added Index page

---
+++ SiteOfCurrencies/Data/Models/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SiteOfCurrencies.Data.Models
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
--- SiteOfCurrencies/Startup.cs
+++ SiteOfCurrencies/Startup.cs
@@ -24,11 +24,16 @@ namespace SiteOfCurrencies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            //app.UseStaticFiles();
+            //app.UseMvcWithDefaultRoute();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
