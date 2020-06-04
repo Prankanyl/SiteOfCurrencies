44d9cd78275704bdcaefadc4987ea279f341c025
valera <v752433@icloud.com>
2020-06-04 22:42:39 +0300

    Patch v1.0

--- SiteOfCurrencies/Startup.cs
+++ SiteOfCurrencies/Startup.cs
@@ -20,15 +20,21 @@ namespace SiteOfCurrencies
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStatusCodePages();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            //app.UseMvcWithDefaultRoute();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,21 +42,14 @@ namespace SiteOfCurrencies
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+                {
+                    endpoints.MapRazorPages();
+                }
+            );
         }
     }
 }
